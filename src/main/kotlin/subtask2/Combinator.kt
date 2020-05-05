c56e4b903f4aee44f8b5fbb4b9c2a83efa4f649f
vlad-gal <vlgalathevich@gmail.com>
2020-05-05 20:02:31 +0300

    feat: implement Combinator

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -4,6 +4,31 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val numOfPosters:Long = array[0].toLong()
+        val numOfColors:Long = array[1].toLong()
+        for (i in 0 until numOfColors){
+            if (bin(numOfColors,i) == numOfPosters){
+                return i.toInt()
+            }
+        }
+        return null
+    }
+
+    private fun fact(i: Long):Long = if (i<2){
+        1
+    } else{
+        i*fact(i-1)
+    }
+
+    private fun bin(a:Long,b:Long): Long? = when {
+        a<b -> {
+            null
+        }
+        a == b -> {
+            1
+        }
+        else -> {
+            fact(a)/((fact(a-b))*fact(b))
+        }
     }
 }
